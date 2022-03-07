aa4a374cd481bdc24f182008afeeb3dc9c0ac1bb
Lamkateh <bruno.dilivio@gmail.com>
2022-03-07 11:21:27 +0100

    Adding api controllers

---
+++ app/Http/Controllers/Api/BaseController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+
+class BaseController extends Controller
+{
+    /**
+     * success response method.
+     *
+     * @param $result
+     * @param $message
+     * @param int $code
+     * @return JsonResponse
+     */
+    public function sendResponse($result, $message, int $code = 200): JsonResponse
+    {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message,
+        ];
+
+
+        return response()->json($response, $code);
+    }
+
+
+    /**
+     * return error response.
+     *
+     * @param $error
+     * @param array $errorMessages
+     * @param int $code
+     * @return JsonResponse
+     */
+    public function sendError($error, $errorMessages = [], int $code = 404): JsonResponse
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        if (!empty($errorMessages)) {
+            $response['data'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+}
