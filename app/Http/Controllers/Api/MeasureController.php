82f0e2db73f4b0654a9322a2a60206af2ea29e00
Lamkateh <bruno.dilivio@gmail.com>
2022-04-08 15:20:37 +0200

    Adding post measures

--- app/Http/Controllers/Api/MeasureController.php
+++ app/Http/Controllers/Api/MeasureController.php
@@ -2,83 +2,33 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Resources\MeasureResource;
+use App\Models\Group;
+use App\Models\Measure;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MeasureController extends BaseController
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        //
-    }
+        $request->validate([
+            'values' => ['required'],
+        ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+        $group = Group::where('start', '<=', now())->where('end', '>=', now())->first();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+        $measure = Measure::create([
+            "values" => $request->get('values'),
+            'group_id' => $group->id,
+        ]);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return $this->sendResponse(new MeasureResource($measure), 'Measure created successfully.');
     }
 }
